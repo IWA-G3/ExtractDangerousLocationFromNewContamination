cf511287c5c1aed95f5f48153f0411fd89b5aff0
Arthur Leblanc <arthurleblanc11@gmail.com>
2020-11-20 16:42:49 +0100

    setup repo

---
+++ src/main/java/polytech/group3/iwa/ExtractDangerousLocationFromNewContamination/ExtractDangerousLocationFromNewContaminationApplication.java
@@ -0,0 +1,11 @@
+package polytech.group3.iwa.ExtractDangerousLocationFromNewContamination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractDangerousLocationFromNewContaminationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractDangerousLocationFromNewContaminationApplication.class, args);}
+
+}
